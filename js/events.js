2cc148d03ac71a1230395339af6508cdb9472faf
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-19 00:41:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,7 +9,7 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keydown', function(key) {
-    if(key.which === 83) { 
+    if(key.which === 71) { 
       alert(`you pressed G key`)
     }
   })
