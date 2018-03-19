286d3f586b851312a2fd6270cd691ba2672c8edc
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-19 00:43:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,6 +15,11 @@ function pressIt() {
   })
 }
 
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 $(document).ready(function(){
 
 // call functions here
